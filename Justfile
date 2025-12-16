c7ae24e063c67a80371b27bc0b7f7a829ec5d0a3
skunkworks <adam@hack.bg>
2025-12-16 18:58:54 +0200

    wip: feat(btc): standalone CMR-to-P2TR

--- Justfile
+++ Justfile
@@ -1,16 +1,8 @@
+t:
+  @just --list
 wasm:
-  CC=emcc EMCC_CFLAGS=--no-entry cargo build --target wasm32-unknown-emscripten
-wasm-dev:
-  CC=emcc EMCC_CFLAGS=--no-entry cargo build --target wasm32-unknown-emscripten --release
-wasm-pack-unknown:
-  time wasm-pack build --release --target web
-  rm -v pkg/package.json pkg/.gitignore
-wasm-pack-unknown-dev:
   time wasm-pack build --debug --no-opt --target web
   rm -v pkg/package.json pkg/.gitignore
-wasm-pack-emscripten:
-  CC=emcc time wasm-pack build --release --target web -- --target wasm32-unknown-emscripten
-  rm -v pkg/package.json pkg/.gitignore
-wasm-pack-emscripten-dev:
-  CC=emcc time wasm-pack build --debug --target web -- --target wasm32-unknown-emscripten
-  rm -v pkg/package.json pkg/.gitignore
+wasm-release:
+  time wasm-pack build --release --target web
+  rm
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 extern crate console_error_panic_hook;
-
 pub(crate) use wasm_bindgen::prelude::*;
-
-pub(crate) use std::collections::HashMap;
-
-pub(crate) use js_sys::{
-    JsString, Object, Error, Reflect, Boolean, Array, JSON
-};
-
+pub(crate) use js_sys::{JsString, Object, Error, Reflect, Boolean, JSON};
 pub(crate) use simplicityhl::{
     dummy_env,
     Arguments, CompiledProgram, SatisfiedProgram, WitnessValues, Value,
@@ -18,18 +11,10 @@ pub(crate) use simplicityhl::{
         jet::Elements,
     },
 };
-
-pub(crate) use elements::{
-    taproot::{LeafVersion, TaprootBuilder, TaprootSpendInfo},
-    Address, AddressParams, Script, secp256k1_zkp as secp256k1,
-};
-
+pub(crate) type Maybe<T> = Result<T, Error>;
 macro_rules! attempt { ($expr:expr) => { $expr.map_err(|e|Error::new(&format!("{e}")))? } }
-
 macro_rules! get { ($obj:expr, $key:expr) => { Reflect::get(&$obj, &JsString::from($key).into())? } }
-
 macro_rules! set { ($obj:expr, $key:expr, $value:expr) => {{ Reflect::set(&$obj, &JsString::from($key).into(), &$value.into())?; }} }
-
 macro_rules! each {
     ($obj:expr => |$key:ident,$val:ident|$cb:expr) => {{
         Object::entries(&$obj.into()).for_each(&mut |entry, _, _| {
@@ -41,7 +26,6 @@ macro_rules! each {
     }}
 }
 
-pub(crate) type Maybe<T> = Result<T, Error>;
-
 mod compile; pub use self::compile::*;
-mod deploy; pub use self::deploy::*;
+
+mod deploy;  pub use self::deploy::*;
