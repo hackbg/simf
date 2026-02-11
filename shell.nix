d324386535f1044bbc1317a898f14f0f158bffe6
Hack.bg Admin <hello@hack.bg>
2026-02-11 02:49:20 +0000

    feat(shell): add deno for running tests

--- shell.nix
+++ shell.nix
@@ -2,10 +2,11 @@
 {pkgs?import<nixpkgs>{}}: let
   sh = name: nativeBuildInputs: opts: pkgs.mkShell ({ inherit name nativeBuildInputs; } // opts);
 in sh "fadroma-simf" [
-  pkgs.lld
-  pkgs.cloc
-  pkgs.binaryen
-  pkgs.python3
   pkgs.bacon
+  pkgs.binaryen
+  pkgs.cloc
+  pkgs.deno
+  pkgs.lld
+  pkgs.python3
   pkgs.rustup
 ] {}
